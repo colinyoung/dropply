9a1facc2bf81fd4ad844d4f5dce1eb24be2a80f9
Colin Young <me@colinyoung.name>
2010-12-04 19:18:35 -0800

    added find_for_facebook to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,26 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :first_name, :last_name
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name
+  
+  def self.find_for_facebook_oauth(data, signed_in_resource=nil)
+      token = data['credentials']['token']
+      email = data["extra"]["user_hash"]["email"]
+      if user = User.find_by_email(email)
+        user.authcode_facebook = token
+        user.save
+        user
+      else # Create an user with a stub password.
+        User.create!(
+          :email => email, 
+          :password => Devise.friendly_token[0,20],
+          :first_name => data["user_info"]["first_name"],
+          :last_name => data["user_info"]["last_name"]
+        )
+      end
+    end
+  
 end
