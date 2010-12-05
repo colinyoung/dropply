91ded42ebe0a59a4c6b5f62b826acf1ede2ebc4e
Colin Young <me@colinyoung.name>
2010-12-04 16:59:18 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dropply::Application.config.secret_token = '8e430dd7327ef8f35ee7a140e258c87f616f3255151e517cd859ac6a2a29d83ed8eb95fa59851ed0ab43d53663714369a3a1f5ce7530f272fc6d403c9a16ea55'
