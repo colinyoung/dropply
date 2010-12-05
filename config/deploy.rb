16e2ac9a334863bf9c98819ef3c6e9cbb1cc1d67
Colin Young <me@colinyoung.name>
2010-12-04 17:22:26 -0800

    added database.yml to deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,6 +13,7 @@ role :app, "dropp.ly"                          # This may be the same as your `W
 role :db,  "dropp.ly", :primary => true # This is where Rails migrations will run
 role :db,  "dropp.ly"
 
+after :deploy, "deploy:copy_database_yml"
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
@@ -22,5 +23,8 @@ namespace :deploy do
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+  task :copy_database_yml do
+    run "cp /var/www/public_html/dropp.ly/database.yml #{File.join(current_path)}/config"
   end
 end
